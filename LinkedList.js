d600ab3deb2170c71b3c1834a410bcd601a3289b
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-12-12 15:01:08 +0000

    added LinkedList.js

---
+++ LinkedList.js
@@ -0,0 +1,12 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.value = null;
+  }
+  //methods: insertFirst(value), insertLast(value), insert(node1, node2), find(value)
+  //methods: deleteFirst(value), deleteLast(value), delete(value, index)
+}
+
+function main() {
+  let list = new LinkedList();
+}
